cdfac4118c911f8ef53701818ff3a78812302833
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2018-11-10 03:59:43 -0500

    Added ability to add a specific item to the collection.

--- fcman.py
+++ fcman.py
@@ -783,6 +783,9 @@ class Action(object):
         node = self.collection.rootnode
 
         while len(path):
+            if not isinstance(node, Directory):
+                break
+
             for name in node.children:
                 if name == path[0]:
                     node = node.children[name]
@@ -1164,6 +1167,93 @@ class DeleteAction(Action):
 
         self.collection.save()
         return True
+
+
+class AddAction(UpdateAction):
+    """ Action to add a filesystem item to the collection. """
+    ACTION_NAME = "add"
+    ACTION_DESC = "Add a filesystem item to the collection"
+
+    @classmethod
+    def add_arguments(cls, parser):
+        super(AddAction, cls).add_arguments(parser)
+
+        parser.add_argument("-p", "--parents", dest="parents", default=False, action="store_true")
+        parser.add_argument("path")
+
+    def run(self):
+        if not self.load():
+            return False
+
+        path = self.normalize_path(self.options.path)
+        if path is None:
+            return False
+
+        (node, remaining) = self.find_nearest_node(path)
+        if len(remaining) == 0:
+            self.writer.stderr.status(path, "EXISTS")
+            return False
+
+        node = self._handle_parents(node, remaining[0:-1])
+        if node is None:
+            return False
+
+        # Now we want to add the item to the node
+        name = remaining[-1]
+        path = os.path.join(node.path, name)
+
+        if os.path.islink(path):
+            item = Symlink(node, name, "")
+            self.writer.stdout.status(item.prettypath, "ADDED")
+            self.handle_symlink(item)
+        elif os.path.isfile(path):
+            item = File(node, name, 0, 0, "")
+            self.writer.stdout.status(item.prettypath, "ADDED")
+            self.handle_file(item)
+        elif os.path.isdir(path):
+            item = Directory(node, name)
+            self.writer.stdout.status(item.prettypath, "ADDED")
+            if self.options.recurse:
+                self.handle_directory(item)
+        else:
+            return False
+
+        self.collection.save()
+        return True
+
+    def _handle_parents(self, node, parts):
+        """ Create each directory part (only if the given directory actually exists). """
+
+        path = list(node.pathlist)
+        while True:
+            # All nodes should be directories
+            if not isinstance(node, Directory):
+                self.writer.stderr.status(path, "NOTDIRECTORY")
+                return None
+
+            # We can't add if the node's fs component does not exist
+            if not node.exists():
+                self.writer.stderr.status(path, "NOTEXIST")
+                return None
+
+            # If no more parts, return the directory node
+            if len(parts) == 0:
+                return node
+
+            if not self.options.parents:
+                self.writer.stderr.status(path, "NOPARENTS")
+                return None
+
+            # Check if the part is in the directory
+            name = parts.pop(0)
+            path.append(name)
+
+            if name in node.children:
+                # Should never happen since if it did exist, find_nearest_node would have it
+                node = node.children[name]
+            else:
+                node = Directory(node, name)
+                self.writer.stdout.status(node.prettypath, "ADDED")
 
 
 class ExportAction(Action):
