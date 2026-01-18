962c293a4071bb95b7be5dba80958647d37d52e0
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2026-01-18 17:00:01 -0500

    Init action now creates .fcman and staging.xml

--- mrbavii/fcman/actions/action_init.py
+++ mrbavii/fcman/actions/action_init.py
@@ -2,7 +2,7 @@
 # pylint: disable=too-many-lines,missing-docstring,too-many-branches
 
 __author__ = "Brian Allen Vanderburg II"
-__copyright__ = "Copyright (C) 2013-2018 Brian Allen Vanderburg II"
+__copyright__ = "Copyright (C) 2013-202026 Brian Allen Vanderburg II"
 __license__ = "MIT License"
 
 
@@ -24,21 +24,20 @@ class InitAction(ActionBase):
 
     def run(self):
         # This is a special action, collection is not loaded at this point
-        # can't use self.program.file or self.program.collection
+        # can't use self.program.dir or self.program.collection
         coll = collection.Collection()
         coll.set_root(".")
-        coll.set_exportdir(".")
-        if self.options.root is not None:
-            coll.autoroot = self.options.root
 
-        if os.path.exists(self.options.file):
-            self.writer.stderr.status(self.options.file, "EXISTS")
+        if os.path.exists(self.options.dir):
+            self.writer.stderr.status(self.options.dir, "EXISTS")
             return False
 
-        self.writer.stdout.status(self.options.file, "INIT")
+        self.writer.stdout.status(self.options.dir, "INIT")
 
         # Store the collection and file in the program so it will save it
-        self.program.file = self.options.file
+        os.makedirs(self.options.dir)
+
+        self.program.dir = self.options.dir
         self.program.collection = coll
         coll.dirty = True
 
